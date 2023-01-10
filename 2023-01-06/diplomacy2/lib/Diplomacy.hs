8b89908ad8dbb253ab66d87dd599cc51afa76966
Arnaud Bailly <arnaud.oqube@gmail.com>
2023-01-10 14:55:03 +0100

    Add property for basic move

--- 2023-01-06/diplomacy2/lib/Diplomacy.hs
+++ 2023-01-06/diplomacy2/lib/Diplomacy.hs
@@ -2,8 +2,11 @@ module Diplomacy where
 
 import qualified Data.List as List
 
-data Province = Venice | Rome
+data Province = Venice | Rome | Tuscany | Piemont | Puglia | Naples
     deriving (Eq, Show, Ord, Enum, Bounded)
+
+allProvinces :: [Province]
+allProvinces = enumFromTo minBound maxBound
 
 data Unit = Army Province
     deriving (Eq, Show)
@@ -11,14 +14,40 @@ data Unit = Army Province
 data Country = Austria
     deriving (Eq, Show, Ord, Enum, Bounded)
 
-data Order = Move Unit Province
+allCountries :: [Country]
+allCountries = enumFromTo minBound maxBound
+
+data Order = Move Country Unit Province
     deriving (Eq, Show)
 
 type Position = (Country, Unit)
 type GameMap = [(Province, Province)]
 
-solve :: [(Country, Order)] -> [Position] -> GameMap -> [Position]
+adjacent :: GameMap -> Province -> Province -> Bool
+adjacent map p1 p2 = (p1, p2) `elem` map || (p2, p1) `elem` map
+
+neighbours :: Province -> GameMap -> [Province]
+neighbours province = foldr (neighbour province) []
+
+neighbour :: Province -> (Province, Province) -> [Province] -> [Province]
+neighbour p (p1, p2) ps
+    | p == p1 && p2 `notElem` ps = p2 : ps
+    | p == p2 && p1 `notElem` ps = p1 : ps
+    | otherwise = ps
+
+defaultMap :: GameMap
+defaultMap =
+    [ (Venice, Rome)
+    , (Venice, Tuscany)
+    , (Venice, Piemont)
+    , (Rome, Tuscany)
+    , (Rome, Naples)
+    , (Rome, Puglia)
+    , (Naples, Puglia)
+    ]
+
+solve :: [Order] -> [Position] -> GameMap -> [Position]
 solve [] oob _ = oob
-solve ((cty, Move (Army src) dest) : orders) positions map =
+solve (Move cty (Army src) dest : orders) positions map =
     let positions' = List.delete (cty, Army src) positions
      in (cty, Army dest) : solve orders positions' map
--- 2023-01-06/diplomacy2/test/DiplomacySpec.hs
+++ 2023-01-06/diplomacy2/test/DiplomacySpec.hs
@@ -2,10 +2,11 @@ module DiplomacySpec where
 
 import Control.Monad (foldM)
 import Data.Function ((&))
-import Diplomacy (Country (..), GameMap, Order (..), Position, Province (..), Unit (..), solve)
+import Data.List (tails)
+import Diplomacy (Country (..), GameMap, Order (..), Position, Province (..), Unit (..), adjacent, allCountries, allProvinces, defaultMap, neighbours, solve)
 import Test.Hspec (Spec, it, pending, shouldBe)
 import Test.Hspec.QuickCheck (prop)
-import Test.QuickCheck (Arbitrary, Property, arbitrary, counterexample, elements, forAll, oneof, (==>))
+import Test.QuickCheck (Arbitrary (..), Property, counterexample, elements, forAll, oneof, suchThat, (==>))
 import Test.QuickCheck.Gen (Gen)
 
 spec :: Spec
@@ -33,16 +34,18 @@ spec = do
             map = [(Venice, Rome)]
         solve noOrders oob map `shouldBe` oob
 
-    prop "solve a single order on an non-empty oob and map" prop_solve_single_move_order
+    prop "solve a single move order on an non-empty oob and map" prop_solve_single_move_order
+    prop "multiple conflicting orders cancel themselves" prop_conflicting_moves_cancel_themselves
 
 newtype Oob = Oob [Position]
     deriving (Eq, Show)
 
 instance Arbitrary Oob where
     arbitrary = Oob <$> foldM genUnit [] (enumFromTo minBound maxBound)
+    shrink (Oob pos) = Oob <$> tails pos
 
 instance Arbitrary Country where
-    arbitrary = elements $ enumFromTo minBound maxBound
+    arbitrary = elements allCountries
 
 genUnit :: [Position] -> Province -> Gen [Position]
 genUnit positions province = do
@@ -56,26 +59,39 @@ prop_solve_single_move_order :: Oob -> Property
 prop_solve_single_move_order (Oob oob) =
     not (null oob)
         ==> forAll (genOneMove oob defaultMap)
-        $ \move@(cty, Move (Army src) dest) ->
+        $ \move@(Move cty (Army src) dest) ->
             let oob' = solve [move] oob defaultMap
              in (cty, Army dest) `elem` oob'
                     && (cty, Army src) `notElem` oob'
                         & counterexample ("new oob: " <> show oob')
 
-genOneMove :: [Position] -> GameMap -> Gen (Country, Order)
+prop_conflicting_moves_cancel_themselves :: ConflictingMoves -> Property
+prop_conflicting_moves_cancel_themselves (ConflictingMoves oob move1 move2) =
+    let oob' = solve [move1, move2] oob defaultMap
+     in oob == oob'
+            & counterexample ("new oob: " <> show oob')
+
+data ConflictingMoves = ConflictingMoves [Position] Order Order
+    deriving (Eq, Show)
+
+instance Arbitrary Province where
+    arbitrary = elements allProvinces
+
+instance Arbitrary ConflictingMoves where
+    arbitrary = do
+        dest <- arbitrary
+        src1 <- arbitrary `suchThat` \p -> (p /= dest) && adjacent defaultMap p dest
+        src2 <- arbitrary `suchThat` \p -> (p /= dest) && adjacent defaultMap p dest && p /= src1
+        cty1 <- arbitrary
+        cty2 <- arbitrary
+        pure $
+            ConflictingMoves
+                [(cty1, Army src1), (cty2, Army src2)]
+                (Move cty1 (Army src1) dest)
+                (Move cty2 (Army src2) dest)
+
+genOneMove :: [Position] -> GameMap -> Gen Order
 genOneMove positions map = do
     (cty, unit@(Army pos)) <- elements positions
     tgt <- elements $ neighbours pos map
-    pure (cty, Move unit tgt)
-
-neighbours :: Province -> GameMap -> [Province]
-neighbours province = foldr (neighbour province) []
-
-neighbour :: Province -> (Province, Province) -> [Province] -> [Province]
-neighbour p (p1, p2) ps
-    | p == p1 && p2 `notElem` ps = p2 : ps
-    | p == p2 && p1 `notElem` ps = p1 : ps
-    | otherwise = ps
-
-defaultMap :: GameMap
-defaultMap = [(Venice, Rome)]
+    pure $ Move cty unit tgt
