4098c1733dbf12aa7ebd7c884d72edfda38b5780
IOG Engineering <engineering@iohk.io>
2023-01-20 16:29:53 +0000

    Implemented format number

--- 2023-01-20/lambda/src/main.rs
+++ 2023-01-20/lambda/src/main.rs
@@ -2,25 +2,47 @@ fn main() {
     println!("Hello, world!");
 }
 
-struct Lambda {}
-
-fn format_lambda(lambda : Lambda) -> String {
-  "".to_string()
+enum Lambda {
+    Num(i64),
+    Empty,
 }
 
-#[test]
-fn format_an_empty_lamnbda() {
-    assert!(format_lambda(Lambda{}) == "");
+#[cfg(test)]
+#[macro_use]
+extern crate quickcheck;
+
+#[cfg(test)]
+mod tests {
+
+    use crate::Lambda;
+
+    fn format_lambda(lambda: Lambda) -> String {
+        match lambda {
+            Lambda::Empty => "".to_string(),
+            Lambda::Num(x) => x.to_string(),
+        }
+    }
+
+    #[test]
+    fn format_an_empty_lamnbda() {
+        assert!(format_lambda(Lambda::Empty) == "");
+    }
+
+    quickcheck! {
+        fn format_a_number(x: i64) -> bool {
+            format_lambda(Lambda::Num(x)) == x.to_string()
+        }
+    }
 }
 
-// Acceptance 
+// Acceptance
 // fn lambda(input: &str) -> String {
 // //  target/goal is to write a REPL ->
 //     let expr = parse(input);
 //     let result = eval(expr);
 //     format_lambda(result)
 // }
-// 
+//
 // #[test]
 // fn if_then_else() {
 //     assert!(lambda("((lambda (a b) a) 1 2)") == "1");
