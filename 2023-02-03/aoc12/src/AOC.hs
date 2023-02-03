df007127437f29b2ef6294d35984bbee9c9e0e11
IOG Engineering <engineering@iohk.io>
2023-02-03 16:06:29 +0000

    working

--- 2023-02-03/aoc12/src/AOC.hs
+++ 2023-02-03/aoc12/src/AOC.hs
@@ -3,4 +3,4 @@ module AOC where
 type Position = (Int, Int, Char)
 
 distance :: Position -> Position -> Int
-distance (x, _, _) (x', y', _) = abs (x' - x) + y'
+distance (x, y, _) (x', y', _) = abs (x' - x) + abs (y - y')
