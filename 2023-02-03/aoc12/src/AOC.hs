3412d37b23e681c48cd4ca306e8006c2c37e54ed
IOG Engineering <engineering@iohk.io>
2023-02-03 16:18:16 +0000

    working

--- 2023-02-03/aoc12/src/AOC.hs
+++ 2023-02-03/aoc12/src/AOC.hs
@@ -4,12 +4,10 @@ type Position = (Int, Int, Char)
 
 distance :: Position -> Position -> Int
 distance (x, y, c) (x', y', c') =
-    let z = case c of
+    let zIndex char = case char of
             'S' -> 'a'
             'E' -> 'z'
-            _ -> c
-        z' = case c' of
-            'S' -> 'a'
-            'E' -> 'z'
-            _ -> c'
+            _ -> char
+        z = zIndex c
+        z' = zIndex c'
      in abs (x' - x) + abs (y - y') + abs (fromEnum z - fromEnum z')
