d20c66719734fb1f6df0f10c351ed14b2b7f6f05
IOG Engineering <engineering@iohk.io>
2023-02-03 16:20:06 +0000

    working

--- 2023-02-03/aoc12/test/AOCSpec.hs
+++ 2023-02-03/aoc12/test/AOCSpec.hs
@@ -22,15 +22,17 @@ import Test.Hspec (Spec, describe, it, pending, shouldBe)
 --    take a position and a move -> compute positiong and a move
 
 spec :: Spec
-spec = describe "distance" $ do
-    it "between 2 positions" $ do
-        distance (0, 0, 'a') (1, 0, 'a') `shouldBe` 1
-        distance (0, 0, 'a') (2, 0, 'a') `shouldBe` 2
-        distance (1, 0, 'a') (4, 0, 'a') `shouldBe` 3
-        distance (5, 0, 'a') (1, 0, 'a') `shouldBe` 4
-        distance (3, 0, 'a') (1, 3, 'a') `shouldBe` 5
-        distance (3, 1, 'a') (1, 5, 'a') `shouldBe` 6
-        distance (3, 1, 'a') (1, 5, 'b') `shouldBe` 7
-        distance (3, 1, 'S') (1, 5, 'c') `shouldBe` 8
-        distance (3, 1, 'c') (1, 5, 'S') `shouldBe` 8
-        distance (3, 1, 'y') (1, 7, 'E') `shouldBe` 9
+spec = do
+    describe "distance" $ do
+        it "between 2 positions" $ do
+            distance (0, 0, 'a') (1, 0, 'a') `shouldBe` 1
+            distance (0, 0, 'a') (2, 0, 'a') `shouldBe` 2
+            distance (1, 0, 'a') (4, 0, 'a') `shouldBe` 3
+            distance (5, 0, 'a') (1, 0, 'a') `shouldBe` 4
+            distance (3, 0, 'a') (1, 3, 'a') `shouldBe` 5
+            distance (3, 1, 'a') (1, 5, 'a') `shouldBe` 6
+            distance (3, 1, 'a') (1, 5, 'b') `shouldBe` 7
+            distance (3, 1, 'S') (1, 5, 'c') `shouldBe` 8
+            distance (3, 1, 'c') (1, 5, 'S') `shouldBe` 8
+            distance (3, 1, 'y') (1, 7, 'E') `shouldBe` 9
+            distance (3, 1, 'E') (1, 7, 'y') `shouldBe` 9
