1e7e31c1eb45b73602450b8b643a9ec72a2c4950
IOG Engineering <engineering@iohk.io>
2023-02-03 15:43:04 +0000

    working

--- 2023-02-03/aoc12/test/AOCSpec.hs
+++ 2023-02-03/aoc12/test/AOCSpec.hs
@@ -10,5 +10,15 @@ import Test.Hspec (Spec, it, pending, shouldBe)
 --
 -- result = 31 steps
 
+-- algo:
+--  looks like a A*?
+--  shortest path in a graph -> needs backtracking + evaluation
+--  need to compute distance to goal -> in 3D -> manhattan distance
+--  evaluate quality of move?
+--
+-- 1. how to representin the input ?
+-- 2. compute one move
+--    take a position and a move -> compute positiong and a move
+
 spec :: Spec
 spec = it "does something" $ 1 `shouldBe` 1
