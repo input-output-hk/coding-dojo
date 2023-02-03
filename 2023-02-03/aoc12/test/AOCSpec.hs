aa6b6d0c27bab69fedd36a1f9f6a8b10d96da9df
IOG Engineering <engineering@iohk.io>
2023-02-03 15:36:00 +0000

    working

--- 2023-02-03/aoc12/test/AOCSpec.hs
+++ 2023-02-03/aoc12/test/AOCSpec.hs
@@ -2,5 +2,11 @@ module AOCSpec where
 
 import Test.Hspec (Spec, it, pending, shouldBe)
 
+-- Sabqponm
+-- abcryxxl
+-- accszExk
+-- acctuvwj
+-- abdefghi
+
 spec :: Spec
 spec = it "does something" $ 1 `shouldBe` 1
