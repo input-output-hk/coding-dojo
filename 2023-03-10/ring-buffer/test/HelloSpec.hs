c4dedce44c174e4a7b42190e2d4282ce0c3ec0b4
IOG Engineering <engineering@iohk.io>
2023-03-10 17:07:12 +0000

    Trying to implement the "right thing"

--- 2023-03-10/ring-buffer/test/HelloSpec.hs
+++ 2023-03-10/ring-buffer/test/HelloSpec.hs
@@ -2,19 +2,20 @@
 
 module HelloSpec where
 
-import Data.IORef (IORef, newIORef, writeIORef, readIORef)
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
+import Data.Vector.Mutable as V (IOVector, length, read, replicate, write)
+
 import Hello (greetings)
-import Test.Hspec (Spec, it, shouldBe, shouldReturn)
+import Test.Hspec (Spec, describe, it, shouldBe, shouldReturn)
 
 -- data RingBuffer e
 --
 -- -- create a new ringbuffer with the given capacity
-data RingBuffer e
-    = RingBuffer { capacity :: Int, buffer :: IORef (Maybe e) }
+data RingBuffer e = RingBuffer {capacity :: Int, buffer :: IOVector e}
 
 newBuffer :: Int -> IO (RingBuffer e)
 newBuffer capacity = do
-    x <- newIORef Nothing
+    x <- V.replicate capacity 0
     pure (RingBuffer capacity x)
 
 --
@@ -26,20 +27,19 @@ newBuffer capacity = do
 --
 -- -- returns true if it could push, false if it couldn't (no capacity)
 push :: e -> RingBuffer e -> IO Bool
-
-push x (RingBuffer capacity ioref) 
-  | capacity == 0 = return False 
-  | otherwise = do
-      writeIORef ioref (Just x)
-      pure True
+push x (RingBuffer capacity ioref)
+    | capacity == 0 = return False
+    | otherwise = do
+        writeIORef ioref (Just x)
+        pure True
 
 --
 -- -- pop an element off the ringbuffer, returns Nothing if there isn't one
 pop :: RingBuffer e -> IO (Maybe e)
-pop (RingBuffer _ ioref) = do 
-  v <- readIORef ioref
-  writeIORef ioref Nothing
-  return v
+pop (RingBuffer _ ioref) = do
+    v <- readIORef ioref
+    writeIORef ioref Nothing
+    return v
 
 spec :: Spec
 spec = do
@@ -54,14 +54,19 @@ spec = do
 
     it "a thing should be gone after it is popped" $ do
         (buffer :: RingBuffer Int) <- newBuffer 42
-        push 79 buffer 
+        push 79 buffer
         pop buffer
         pop buffer `shouldReturn` Nothing
 
-    it "push over capacity" $ do
-        (buffer :: RingBuffer Int) <- newBuffer 0
-        push 79 buffer `shouldReturn` False
+    describe "push over capacity" $ do
+        it "when 0" $ do
+            (buffer :: RingBuffer Int) <- newBuffer 0
+            push 79 buffer `shouldReturn` False
 
+        it "when 1" $ do
+            (buffer :: RingBuffer Int) <- newBuffer 1
+            push 97 buffer
+            push 79 buffer `shouldReturn` False
 
 -- Run me with `cabal test --test-show-details=direct`
--- 
+--
