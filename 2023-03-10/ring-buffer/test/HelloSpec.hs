8b4405adb200851161054ff67164188ec63bf1c0
IOG Engineering <engineering@iohk.io>
2023-03-10 16:19:11 +0000

    push and pop works

--- 2023-03-10/ring-buffer/test/HelloSpec.hs
+++ 2023-03-10/ring-buffer/test/HelloSpec.hs
@@ -1,31 +1,49 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module HelloSpec where
 
-import Test.Hspec(Spec, shouldBe)
-import Hello(greetings)
-
+import Data.IORef (IORef, newIORef, writeIORef, readIORef)
+import Hello (greetings)
+import Test.Hspec (Spec, it, shouldBe, shouldReturn)
 
 -- data RingBuffer e
--- 
+--
 -- -- create a new ringbuffer with the given capacity
--- newBuffer :: Int -> IO (RingBuffer e)
--- 
+data RingBuffer e
+    = RingBuffer (IORef (Maybe e))
+
+newBuffer :: Int -> IO (RingBuffer e)
+newBuffer _ = do
+    x <- newIORef Nothing
+    pure (RingBuffer x)
+
+--
 -- -- get the capacity of the ringbuffer
 -- capacity :: RingBuffer e -> IO Int
--- 
+--
 -- -- get the amount of free space in the ringbuffer
 -- freeSpace :: RingBuffer e -> IO Int
--- 
+--
 -- -- returns true if it could push, false if it couldn't (no capacity)
--- push :: e -> RingBuffer e -> IO Bool
--- 
+push :: e -> RingBuffer e -> IO Bool
+push x (RingBuffer ioref) = do
+  writeIORef ioref (Just x)
+  pure True
+
+--
 -- -- pop an element off the ringbuffer, returns Nothing if there isn't one
--- pop :: RingBuffer e -> IO (Maybe e)
+pop :: RingBuffer e -> IO (Maybe e)
+pop (RingBuffer ioref) = readIORef ioref
 
 spec :: Spec
 spec = do
-  it "pop on new buffer yields Nothing" $ do
-    buffer <- newBuffer 42
-    pop buffer `shouldReturn` Nothing 
-    
+    it "pop on new buffer yields Nothing" $ do
+        (buffer :: RingBuffer Int) <- newBuffer 42
+        pop buffer `shouldReturn` Nothing
+
+    it "push and pop the same thing should come back" $ do
+        (buffer :: RingBuffer Int) <- newBuffer 42
+        push 79 buffer `shouldReturn` True
+        pop buffer `shouldReturn` Just 79
 
 -- Run me with `cabal test --test-show-details=direct`
