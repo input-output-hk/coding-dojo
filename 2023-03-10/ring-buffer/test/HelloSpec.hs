6e7034bf75f87a6c8ddaab1bc8a261750a6dda76
IOG Engineering <engineering@iohk.io>
2023-03-10 16:34:19 +0000

    cheating on capacity

--- 2023-03-10/ring-buffer/test/HelloSpec.hs
+++ 2023-03-10/ring-buffer/test/HelloSpec.hs
@@ -10,12 +10,12 @@ import Test.Hspec (Spec, it, shouldBe, shouldReturn)
 --
 -- -- create a new ringbuffer with the given capacity
 data RingBuffer e
-    = RingBuffer (IORef (Maybe e))
+    = RingBuffer { capacity :: Int, buffer :: IORef (Maybe e) }
 
 newBuffer :: Int -> IO (RingBuffer e)
-newBuffer _ = do
+newBuffer capacity = do
     x <- newIORef Nothing
-    pure (RingBuffer x)
+    pure (RingBuffer capacity x)
 
 --
 -- -- get the capacity of the ringbuffer
@@ -26,14 +26,20 @@ newBuffer _ = do
 --
 -- -- returns true if it could push, false if it couldn't (no capacity)
 push :: e -> RingBuffer e -> IO Bool
-push x (RingBuffer ioref) = do
-  writeIORef ioref (Just x)
-  pure True
+
+push x (RingBuffer capacity ioref) 
+  | capacity == 0 = return False 
+  | otherwise = do
+      writeIORef ioref (Just x)
+      pure True
 
 --
 -- -- pop an element off the ringbuffer, returns Nothing if there isn't one
 pop :: RingBuffer e -> IO (Maybe e)
-pop (RingBuffer ioref) = readIORef ioref
+pop (RingBuffer _ ioref) = do 
+  v <- readIORef ioref
+  writeIORef ioref Nothing
+  return v
 
 spec :: Spec
 spec = do
@@ -46,4 +52,16 @@ spec = do
         push 79 buffer `shouldReturn` True
         pop buffer `shouldReturn` Just 79
 
+    it "a thing should be gone after it is popped" $ do
+        (buffer :: RingBuffer Int) <- newBuffer 42
+        push 79 buffer 
+        pop buffer
+        pop buffer `shouldReturn` Nothing
+
+    it "push over capacity" $ do
+        (buffer :: RingBuffer Int) <- newBuffer 0
+        push 79 buffer `shouldReturn` False
+
+
 -- Run me with `cabal test --test-show-details=direct`
+-- 
